db9a50f64bd1df6c1bf91845d6c375e41366ee8d
bekreth <bekreth@gmail.com>
2021-11-22 15:25:08 -0800

    Setup 25 days worth of cargo files

--- day23/src/main.rs
+++ day23/src/main.rs
@@ -1,3 +1,8 @@
+use std::fs;
+
 fn main() {
-    println!("Hello, world!");
+    let filename = "input_data.txt";
+    let contents = fs::read_to_string(filename)
+        .expect("Unable to read file");
+    println!("{}", contents);
 }
