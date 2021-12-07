f4652b072cb1137923003e5c3fb68dbba3d17455
bekreth <bekreth@gmail.com>
2021-12-06 16:41:32 -0800

    Cleaned up day 6

--- day06/src/main.rs
+++ day06/src/main.rs
@@ -1,12 +1,3 @@
-use std::thread::JoinHandle;
-use std::sync::mpsc;
-use std::fs;
-use std::env;
-use chrono::Utc;
-use threadpool::ThreadPool;
-extern crate chrono;
-extern crate threadpool;
-
 const SAMPLE: [u8; 5] = [3,4,3,1,2];
 const INPUT: [u8; 300] = [
     5,3,2,2,1, 1,4,1,5,5, 1,3,1,5,1, 2,1,4,1,2, // 20
@@ -63,7 +54,7 @@ fn main() {
         &mut fish_7,
         &mut fish_8,
     ];
-    let mut collection = INPUT.iter()
+    let collection = INPUT.iter()
         .map(LanternFish::new)
         .fold(&mut empty_start_vector,|collector, fish| {
             collector[fish.age as usize].count += fish.count;
@@ -102,97 +93,3 @@ fn main() {
 
     println!("Gold: {}", gold);
 }
-
-/*
-fn from_6() -> u128 {
-    return 3_989_468_462
-}
-
-struct Work<'a> { 
-    fish: Box<&'a mut LanternFish>,
-    count_down: u8
-}
-*/
-
-/*
-fn new_work<'a>(input: u8) -> Work<'a> {
-    return Work {
-        fish: Box::new(&mut LanternFish{age: input}),
-        count_down: 100
-    }
-}
-*/
-
-/*
-fn main() {
-    let (work_queue, work_dequeue) = mpsc::channel::<Work>();
-    let (count_out, count_in) = mpsc::channel::<()>(); 
-
-    let pool = ThreadPool::with_name("worker".into(), 10);
-    let mut workers = vec![];
-
-    for i in 0..10 {
-        let work_out = work_queue.clone();
-        let (work_sender, work_reciever) = mpsc::channel::<Work>();
-        let count_exporter = count_out.clone();
-        pool.execute(move || {
-            for work in work_reciever.iter() {
-                if work.count_down == 0 {
-                    count_exporter.send(()).expect("Failed to send count")
-                }
-                match work.fish.age_down() {
-                    Some(mut new_fish) => {
-                        let new_work = Work {
-                            fish: Box::new(&mut new_fish),
-                            count_down: work.count_down-1,
-                        };
-                        work_out.send(new_work);
-                    },
-                    None => (),
-                }
-                work_out.send(Work{
-                    fish: work.fish, 
-                    count_down: work.count_down-1
-                }).expect("Failed to send fish")
-            }
-        });
-        workers.push(work_sender);
-    }
-
-
-    let fan_out = std::thread::spawn(move || {
-        let mut pool_counter = 0;
-        for work in work_dequeue {
-            workers[pool_counter].send(work).expect("");
-            pool_counter += 1;
-            pool_counter %= 10;
-        }
-    });
-
-    let count_fish = std::thread::spawn(move || {
-        let mut output: u128 = 0;
-        for i in count_in {
-            output += 1;
-        }
-        return output;
-    });
-
-    // SAMPLE.iter()
-        // .map(|age| LanternFish{age: *age})
-        // .map(|fish| Work{fish: &mut fish, count_down: 80})
-        // .for_each(|work| work_queue.send(work).expect("Failed to send work"));
-    
-    for v in SAMPLE.iter() {
-        // let fish: LanternFish = &mut LanternFish{age: *v};
-        // let work = new_work(*v);
-        // work_queue.send(work).expect("msg: &str");
-    }
-
-    pool.join();
-    fan_out.join();
-
-    let total = count_fish.join();
-    println!("Fish counted: {}", total.expect("Failed to count fish"));
-
-}
-*/
