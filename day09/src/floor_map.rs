0fe4cc5cabcedf1055c95d9560b8dc3c4fc3bedd
bekreth <bekreth@gmail.com>
2021-12-14 10:56:56 -0800

    Solved day9

--- day09/src/floor_map.rs
+++ day09/src/floor_map.rs
@@ -1,8 +1,5 @@
-use std::fmt::Write as FmtWrite;
-use std::io::Write as IoWrite;
-
 pub struct FloorMap {
-    data: Vec<Vec<u16>>
+    data: Vec<Vec<u8>>
 }
 
 impl FloorMap {
@@ -10,84 +7,83 @@ impl FloorMap {
         let map_data = content.lines()
             .map(|line| {
                 line.chars()
-                    .map(|c| c.to_digit(10).expect("failed to parse char") as u16)
-                    .collect::<Vec<u16>>()
+                    .map(|c| c.to_digit(10).expect("failed to parse char") as u8)
+                    .collect::<Vec<u8>>()
             })
-            .collect::<Vec<Vec<u16>>>();
+            .collect::<Vec<Vec<u8>>>();
         FloorMap {
             data: map_data,
         }
     }
 
-    pub fn find_low_points(self) -> Vec<(usize, usize, u16)> {
+    pub fn find_low_points(&self) -> Vec<(usize, usize, u8)> {
         let output = self.data.iter()
             .enumerate()
             .map(|(i, vec_row)| {
-                let output: Vec<(usize, usize, u16)> = vec_row.iter()
+                let output: Vec<(usize, usize, u8)> = vec_row.iter()
                     .enumerate()
                     .filter_map(|(j, _)| is_local_minimum(&self.data, i, j))
-                    .collect::<Vec<(usize, usize, u16)>>();
+                    .collect::<Vec<(usize, usize, u8)>>();
                 output
             })
             .flatten()
-            .collect::<Vec<(usize, usize, u16)>>();
+            .collect::<Vec<(usize, usize, u8)>>();
 
         return output;
     }
 
-    pub fn calculate_basin() -> u16 {
-
-        todo!()
+    pub fn calculate_basin(&self, i: usize, j: usize) -> Vec<(usize, usize, u8)> {
+        let value = self.data[i][j];
+        let collector = vec![(i, j, value)];
+        let mut child_values: Vec<(usize, usize, u8)> = scan_adjacent(&self.data, i, j)
+            .iter()
+            .filter(|(_, _, target)| value < *target && *target != 9)
+            .map(|(i, j, _)| self.calculate_basin(*i, *j))
+            .flatten()
+            .fold(collector, |mut acc, point| {
+                acc.push(point);
+                acc
+            });
+        child_values.sort();
+        child_values.dedup();
+        return child_values;
     }
-
 }
 
-fn is_local_minimum(data: &Vec<Vec<u16>>, i: usize, j: usize) -> Option<(usize, usize, u16)> {
+fn is_local_minimum(data: &Vec<Vec<u8>>, i: usize, j: usize) -> Option<(usize, usize, u8)> {
     let value = data[i][j];
-    let mut writer = String::new();
+    match scan_adjacent(data, i, j)
+        .iter()
+        .find(|(_, _, target)| value >= *target)
+    {
+        Some(_) => None,
+        None => Some((i, j, value)),
+    }
+}
+
+fn scan_adjacent(data: &Vec<Vec<u8>>, i: usize, j: usize) -> Vec<(usize, usize, u8)>{
+    let mut output = vec![];
 
     let height = data[0].len();
     if j > 0 {
         let target = data[i][j - 1];
-        writeln!(&mut writer, "---{}---", target);
-        if value >= target { 
-            return None
-        }
+        output.push((i, j - 1, target));
     }
 
     let length = data.len();
     if i > 0 {
         let target = data[i - 1][j];
-        write!(&mut writer, "{}", target);
-        if value >= target {
-            return None
-        }
-    } else {
-        write!(&mut writer, "-");
+        output.push((i - 1, j, target));
     }
-    write!(&mut writer, "-:{}:-", value);
     if i < length - 1 {
         let target = data[i + 1][j];
-        write!(&mut writer, "{}", target);
-        if value >= target { 
-            return None
-        }
-    } else {
-        write!(&mut writer, "-");
+        output.push((i + 1, j, target));
     }
-    writeln!(&mut writer, "");
 
     if j < height - 1 {
         let target = data[i][j + 1];
-        writeln!(&mut writer, "---{}---", target);
-        if value >= target {
-            return None
-        }
-    } else {
-        writeln!(&mut writer, "-------");
+        output.push((i, j + 1, target));
     }
 
-    // println!("==\n{} ({}, {})\n==", writer, i, j);
-
-    Some((i, j, value))
+    return output;
 }
--- day09/src/main.rs
+++ day09/src/main.rs
@@ -13,12 +13,27 @@ fn main() {
         .expect("failed to read file");
     
     let f_map = FloorMap::new(content);
-    let low_points = f_map.find_low_points();
+    let low_points = &f_map.find_low_points();
     let silver: u16 = low_points.iter()
-        .map(|(i, j, x)| {
-            // println!("{} {}", i, j);
-            *x + 1
-        })
+        .map(|(_, _, x)| *x as u16)
+        .map(|x| x + 1)
         .sum();
     println!("Silver: {}", silver);
+
+    let mut gold_solutions: Vec<usize> = low_points.iter()
+        .map(|(i, j, _)| (&f_map).calculate_basin(*i, *j).len())
+        .collect::<Vec<usize>>();
+
+    gold_solutions.sort();
+    let size = gold_solutions.len();
+    let last_set: &[usize] = gold_solutions
+        .get(size-3..size)
+        .expect("expected 3 last values");
+
+    let gold: usize = last_set.iter()
+        .map(|v| *v)
+        .reduce(|previous, value| previous * value)
+        .expect("Failed to reduce finals");
+
+    println!("Gold: {}", gold);
 }
