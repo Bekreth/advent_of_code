006166da6bd215fba69c0b085cda2c1994a66e1c
bekreth <bekreth@gmail.com>
2022-11-30 22:25:55 -0800

    Third pass

--- 2022/day01/solution.py
+++ 2022/day01/solution.py
@@ -1,7 +1,6 @@
 E=[0]
-for l in open('sample'):
- if l=="\n":E=[0]+E
- else:E[0]+=int(l)
-E.sort()
-o=E[-3:]
-print(f"{o[0]}\n{sum(o)}")
+for l in open('d'):
+ if l=="\n":E+=[0]
+ else:E[-1]+=int(l)
+o=sorted(E)[-3:]
+print(f"{o[2]}\n{sum(o)}")
