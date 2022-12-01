c16c57ed40f5e36bff2a7724c1f7a00ca0f27f3f
bekreth <bekreth@gmail.com>
2022-11-30 21:09:09 -0800

    initial solution day 1

--- 2022/day01/solution.py
+++ 2022/day01/solution.py
@@ -1,1 +1,19 @@
-print("hello")
+import sys
+
+silver = 0
+elves = []
+
+file = open('d')
+lines = file.readlines()
+currentValue = 0
+for line in lines:
+    if line == "\n":
+        elves += [currentValue]
+        currentValue = 0
+    else:
+        currentValue += int(line)
+
+output = sorted(elves)
+print(output[len(elves) - 1])
+print(sum(output[len(elves) - 3:len(elves)]))
+
