406394c63eec137403b79f897f6420d4a2e4ae62
bekreth <bekreth@gmail.com>
2022-11-30 22:10:15 -0800

    Second pass at golf

--- 2022/day01/solution.py
+++ 2022/day01/solution.py
@@ -1,6 +1,7 @@
 E=[0]
 for l in open('sample'):
  if l=="\n":E=[0]+E
- else:E=[E[0]+int(l)]+E[1:]
-o=sorted(E)[-3:]
-print(f"{o[2]}\n{sum(o)}")
+ else:E[0]+=int(l)
+E.sort()
+o=E[-3:]
+print(f"{o[0]}\n{sum(o)}")
