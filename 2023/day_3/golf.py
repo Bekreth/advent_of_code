cce0dd0e748b7c238b40a4d858d9d2240e15befb
bekreth <bekreth@proton.me>
2023-12-03 22:39:26 -0800

    Solution to day 4

--- 2023/day_3/golf.py
+++ 2023/day_3/golf.py
@@ -1,44 +1,28 @@
 E=enumerate
 R=range
 S=[]
-p_parts=[]
+p=[]
 for i,L in E(open("input.txt")):
-    p_num=""
-    for j,C in E(L):
-        if C.isdigit():
-            p_num+=C
-        else:
-            if C!=".":
-                S+=[(i,j,C)]
-            if p_num!="":
-                p_parts+=[[(R(i-1, i+2), R(j-len(p_num)-1, j+1)), int(p_num)]]
-                p_num=""
-
-parts = []
+ N=""
+ for j,C in E(L):
+  if C.isdigit():N+=C
+  else:
+   if C!=".":S+=[(i,j,C)]
+   if N!="":
+    p+=[[R(i-1,i+2),R(j-len(N)-1,j+1),int(N)]]
+    N=""
+P=[]
 Y=Z=0
-for possible_part in p_parts:
-    [(r0, r1), part] = possible_part
-    adjacent = False
-    for s in S:
-        if s[0] in r0:
-            if s[1] in r1:
-                adjacent = True
-    if adjacent:
-        parts += [[(r0, r1), part]]
-        Y += part
-
-for possible_gear in S:
-    if possible_gear[2] != "*":
-        continue
-    adjacent_parts = []
-    for part in parts:
-        [(r0, r1), p_num] = part
-        if possible_gear[0] in r0:
-            if possible_gear[1] in r1:
-                adjacent_parts += [p_num]
-    if len(adjacent_parts) > 1:
-        gear_ratio = 1
-        for gear_part in adjacent_parts:
-            gear_ratio *= gear_part
-        Z += gear_ratio
-print("S:", Y, "\nG:", Z)
+for [a,b,n]in p:
+ for s in S:
+  if s[0]in a and s[1]in b:
+   P+=[[a, b,n]]
+   Y+=n
+   break
+for g in S:
+ A=[N for [a,b,N]in P if g[2]=="*"and g[0]in a and g[1]in b]
+ r=1
+ if len(A)>1:
+  for G in A:r*=G
+  Z+=r
+print("S:",Y,"\nG:",Z)
---
+++ 2023/day_4/solution.py
@@ -0,0 +1,18 @@
+silver = 0
+gold = 0
+
+cards = [1] * 201
+for i, line in enumerate(open("input.txt")):
+    [hand_set, winner_set] = line.split(": ")[1].strip().split(" | ")
+    hand = hand_set.split()
+    winners = winner_set.split()
+    wins = [number for number in hand if number in winners]
+    win_count = len(wins)
+    if win_count == 0: continue
+    value = 2**(win_count - 1)
+    silver += value
+    for j in range(i + 1, i + 1 + win_count):
+        cards[j] += cards[i]
+
+
+print("S:", silver,"\nG", sum(cards))
